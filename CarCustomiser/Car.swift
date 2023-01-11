8e4d0882b2a3d6d1fd2e09fb222f7e6d2c8ec257
Sritej Tummuru <sritej.tummuru@gmail.com>
2023-01-11 22:52:07 +0000

    New Car class created and instantiated.

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Ravindra Tummuru on 11/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+}
